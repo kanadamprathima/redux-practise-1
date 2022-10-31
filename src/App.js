9d56d68db45333838c78e06f8b514a8dd24fc326
prathima <kanadamprathima@gmail.com>
2022-10-31 13:11:00 +0100

    added selector

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import "./App.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { deposit } from "./store/balance/slice";
+import { selectBalance } from "./store/balance/selectors";
 
 function App() {
-  const [balance, setBalance] = useState(0);
+  // const [balance, setBalance] = useState(0);
+  const balance = useSelector(selectBalance);
   const dispatch = useDispatch();
   return (
     <div className="App">
