e72bec83b97f5056daa146d38f84df518e898c10
prathima <kanadamprathima@gmail.com>
2022-10-31 13:58:56 +0100

    added custom btn to deposit n withdraw

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { deposit, withdraw, reset } from "./store/balance/slice";
@@ -6,6 +6,7 @@ import { selectBalance } from "./store/balance/selectors";
 
 function App() {
   // const [balance, setBalance] = useState(0);
+  const [number, setNumber] = useState(0);
   const balance = useSelector(selectBalance);
   const dispatch = useDispatch();
   return (
@@ -14,6 +15,26 @@ function App() {
       <button onClick={() => dispatch(deposit(10))}>deposit</button>
       <button onClick={() => dispatch(withdraw(10))}>Withdraw</button>
       <button onClick={() => dispatch(reset())}>reset</button>
+      <hr />
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => {
+          const parsed = parseInt(e.target.value);
+          if (isNaN(parsed)) {
+            setNumber("");
+          } else {
+            setNumber(parsed);
+          }
+        }}
+      />
+      <button onClick={() => dispatch(deposit(number), setNumber(0))}>
+        {" "}
+        deposit custom amount
+      </button>
+      <button onClick={() => dispatch(withdraw(number), setNumber(0))}>
+        withdraw custom amount
+      </button>
     </div>
   );
 }
